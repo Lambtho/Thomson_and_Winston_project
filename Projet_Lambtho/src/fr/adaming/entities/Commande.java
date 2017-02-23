0b1dc94cfabdaf04db79214edbda7d74c5bd4b3b
inti0221 <inti0221@DESKTOP-EEDAG8V.home>
2017-02-23 11:26:49 +0100

    Premier commit

---
+++ Projet_Lambtho/src/fr/adaming/entities/Commande.java
@@ -0,0 +1,13 @@
+package fr.adaming.entities;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+public class Commande implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private long idCommande;
+	private Calendar dateCommande;
+
+}
