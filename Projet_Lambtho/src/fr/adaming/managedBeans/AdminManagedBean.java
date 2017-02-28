a54ada3b5b58346be0f84d6254c91dd267c58640
inti0221 <inti0221@DESKTOP-EEDAG8V.mshome.net>
2017-02-28 22:57:09 +0100

    Les commentaires, en veux-tu en voila !

--- Projet_Lambtho/src/fr/adaming/managedBeans/AdminManagedBean.java
+++ Projet_Lambtho/src/fr/adaming/managedBeans/AdminManagedBean.java
@@ -31,23 +31,37 @@ public class AdminManagedBean implements Serializable {
 	// Attributs
 	AdminProd administrateur;
 
+	// Injection des varaibles d'implémentation dans le conteneur web
 	@EJB
 	IAdministrateurService adminService;
-
 	@EJB
 	IClientService clientService;
 
+	// Variables
+	
+	//produit (pour mise à jour, création d'un produit, etc.)
 	Produit produit;
+	//Catégorie (idem que pour le produit)
 	Categorie categorie;
+	//Liste de toutes les catégories
 	List<Categorie> listeCategories;
+	//Liste de tous les produits
 	List<Produit> listeProduits;
+	//résultat = 1 => requête executée par le SGBD
 	int resultat;
+	//index quelconque selectionné dans un menu déroulant (string)
 	String indexSelect;
+	//index du produit sélectionné dans un menu déroulant 
 	String indexSelectId;
+	//Index inutile car n'est pas utilisé ...
 	long indexUpdate;
+	//Produit pour la mise à jour (pas très utile non plus)
 	Produit produitUpdate;
 
+	// Constructeur vide
 	public AdminManagedBean() {
+		// Initialisation des variables dans le constructeur vide pour les
+		// initialiser lors du premier appel de la ManagedBean
 		this.administrateur = new AdminProd();
 		this.produit = new Produit();
 		this.categorie = new Categorie();
@@ -55,6 +69,7 @@ public class AdminManagedBean implements Serializable {
 		this.indexSelectId = "0";
 	}
 
+	// Getters & Setters
 	public AdminProd getAdministrateur() {
 		return administrateur;
 	}
@@ -128,113 +143,213 @@ public class AdminManagedBean implements Serializable {
 	}
 
 	// Méthodes
+
+	// Méthode appelée lors de la construction de la page web (premier appel)
 	@PostConstruct
 	public void getAllPostConstr() {
+
+		// Initialise la liste des catégories avec toutes les catégories
 		this.listeCategories = clientService.getAllCategories();
+
+		// Initialise la liste des produits avec tous les produits
 		this.listeProduits = adminService.getAllProductService();
+
+		// Initialise le produit courant avec l'id du produit sélectionné
 		this.produit = listeProduits.get(Integer.parseInt(indexSelectId));
 	}
 
+	// Ajout des produits
 	public String addProduct() {
 
+		// Récupère la catégorie sélectionnée dans le menu déroulant
+		// indexSelect => index de la liste déroulante
+		// listeCategorie => liste des catégories récupérée dans le
+		// postConstruct
 		Categorie cat = new Categorie();
 		cat = this.listeCategories.get(Integer.parseInt(this.indexSelect) - 1);
 
+		// Création d'un nouveau produit avec le nouveau produit saisi dans le
+		// formulaire
 		Produit p = new Produit(this.produit.getDesignation(), this.produit.getDescription(), this.produit.getPrix(),
 				this.produit.getQuantite(), false);
+
+		// Ajout de la catégorie sélectionnée dans le menu déroulant
 		p.setCategorie(cat);
 
+		// Ajout du produit dans la BDD
 		this.resultat = adminService.addProductService(p);
+
+		// Récupération de la liste de tous les produits
 		List<Produit> lp = new ArrayList<Produit>();
 		lp = adminService.getAllProductService();
-		p = lp.get(lp.size() - 1);
 
+		// Ne sert à rien
+		p = lp.get(lp.size() - 1);
 		adminService.updateProductService(p);
 
+		// REtour à l'accueil
 		return "Accueil";
 	}
 
+	// Supprimer un produit
 	public String delProduct() {
+		// Suppression du produit avec son index
 		this.resultat = adminService.delProductService(Long.parseLong(indexSelectId));
+		// Retour à l'accueil
 		return "Accueil";
 	}
 
+	// Mise à jour d'un produit
 	public String updateProduct() {
+		// Récupération du context
 		FacesContext fc = FacesContext.getCurrentInstance();
+		// Récupération de la session à partir du context
 		HttpSession httpSession = (HttpSession) (fc.getExternalContext().getSession(false));
+
+		// Récupération du produit à mettre à jour
 		produitUpdate = (Produit) httpSession.getAttribute("produitUpdate");
+
+		// Modification du produit avec les les valeurs saisies dans le
+		// formulaire
 		this.produitUpdate.setDesignation(this.produit.getDesignation());
 		this.produitUpdate.setDescription(this.produit.getDescription());
 		this.produitUpdate.setPrix(this.produit.getPrix());
 		this.produitUpdate.setQuantite(this.produit.getQuantite());
 		this.produitUpdate.setCategorie(this.categorie);
+
+		// Envoi du produit modifié à la BDD
 		this.resultat = adminService.updateProductService(this.produitUpdate);
+
+		// On met le produit modifié à jour dans la variable d'acces
 		this.produit = this.produitUpdate;
+
+		// Retour à l'accueil
 		return "Accueil";
 	}
 
+	// Trouver un produit avec son index
 	public String findProduct() {
+		// Appel de la méthode getById avec l'index sélectionné dans le
+		// formulaire
 		this.produit = adminService.getByIdProductService(Long.parseLong(indexSelectId));
+
+		// retour à la page qui a appelé la méthode
 		return null;
 	}
 
+	// Méthode pour remplir le formulaire (de modification d'un produit) avec le
+	// produit correspondant à l'index sélectionné
 	public void refreshOneProduct(ValueChangeEvent event) {
-
+		// Conversion de l'index sélectionné (via un évenement et un listenner)
+		// en Long
 		long newId = Long.parseLong((String) event.getNewValue());
+
+		// Récupère le produit correspondant à l'index
 		this.produit = adminService.getByIdProductService(newId);
+
+		// On met le produit récupéré dans la variable accessible (produit à
+		// modifié)
 		this.produitUpdate = new Produit(this.produit.getIdProduit(), this.produit.getDesignation(),
 				this.produit.getDescription(), this.produit.getPrix(), this.produit.getQuantite(), false);
+
+		// On met le produit dans la session
 		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("produitUpdate", produitUpdate);
 	}
 
+	// Mise à jour de la variable catégorie (accessible) à partir de l'index de
+	// la liste déroulante
 	public void refreshOneCategorie(ValueChangeEvent event) {
 
 		this.categorie = this.listeCategories.get(Integer.parseInt((String) event.getNewValue()) - 1);
 	}
 
+	// Méthode de vérification de la connexion
 	public String connexion() {
 
+		// Vérification que le couple login et mot de passe existe dans la BDD
 		this.administrateur = adminService.isExistService(this.administrateur);
+
+		// Si l'identification est bonne
 		if (administrateur != null) {
 			// Créer une session et ajouter l'agent à celle-ci
 			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("agentSession", administrateur);
+
+			// Envoi vers la page d'accueil administrateur
 			return "ConnectSuccess";
-		} else {
+		} else { // Si l'identification est fausse
+			// Ajout du message déreur dans le context
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage("Les informations saisies sont éronnées"));
+
+			// retour dans la page de login
 			return "ConnectFail";
 		}
 	}
 
+	// Méthode de déconnexion
 	public String deconnexion() {
+
+		// On détruit la session
 		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+
+		// Retour à la page de login
 		return "/Admin/login.xhtml";
 	}
 
+	// Méthode pour vérifier que l'utilisateur est bien connecté avant
+	// d'afficher la page
+	//Cette méthode est appellée par le header administrateur
 	public void checkPermissions(ComponentSystemEvent event) throws IOException {
 
+		// Récupère le contexte
 		FacesContext fc = FacesContext.getCurrentInstance();
+
+		// Récupère la session (n'en crée pas une nouvelle si elle n'existe pas)
 		HttpSession httpSession = (HttpSession) (fc.getExternalContext().getSession(false));
+
+		// Création d'une variable administrateur
 		AdminProd adminSession = null;
+
+		// Si la session existe
 		if (httpSession != null) {
+			// On met les infos administrateur dans la variable administrateur
 			adminSession = (AdminProd) httpSession.getAttribute("agentSession");
 		}
+
+		// Si la session n'existe pas
 		if (adminSession == null) {
+
+			// Ajout d'un message dans le context
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage("Vous devez vous connecter pour accéder à l'espace administrateur"));
 
+			// Redirection vers la page de login
 			ExternalContext ec = fc.getExternalContext();
 			ec.redirect(ec.getRequestContextPath() + "/Admin/login.xhtml");
 		}
 	}
 
+	// Verification de la connexion dans la page de login uniquement
 	public void checkPermissionslogin(ComponentSystemEvent event) throws IOException {
+
+		// Récupère le contexte
 		FacesContext fc = FacesContext.getCurrentInstance();
+
+		// Récupère la session
 		HttpSession httpSession = (HttpSession) (fc.getExternalContext().getSession(false));
+
+		// Création d'une variable administrateur
 		AdminProd adminSession = null;
+
+		// Si la session existe
 		if (httpSession != null) {
+			// On met les données de session de l'admin dans la variable
+			// administrateur
 			adminSession = (AdminProd) httpSession.getAttribute("agentSession");
 		}
+
+		// Si la session existe déjà, pas besoin de se logger donc redirection
+		// sur l'accueil
 		if (adminSession != null) {
 			ExternalContext ec = fc.getExternalContext();
 			ec.redirect(ec.getRequestContextPath() + "/Admin/accueilAdmin.xhtml");
--- Projet_Lambtho/src/fr/adaming/managedBeans/ClientManagedBean.java
+++ Projet_Lambtho/src/fr/adaming/managedBeans/ClientManagedBean.java
@@ -24,42 +24,59 @@ public class ClientManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	// Injection des variable d'accès au back-end dans le contener web qui va
+	// les instancier
 	@EJB
 	IClientService clientService;
 
 	@EJB
 	IAdministrateurService adminService;
 
+	// Variables
 	Client client;
 	Produit produit;
 	Categorie categorie;
 	LigneCommande ligneCommande;
 	Commande commande;
 
+	// Listes utilisées dans les menus déroulants
 	List<Categorie> listeCategories;
 	List<Produit> listeProduits;
+	// Listes utilisées pour les liens entre les tables de la BDD
 	List<LigneCommande> listeLignesCmd;
 	List<Produit> listeProduitsCmd;
 	List<Commande> listeCommande;
 
+	// Index des catégories dans les menus déroulant
 	String indexCat;
+	// mot clef recherché
 	String recherche = "";
+	// Numéro de la ligne dans les tableaux de produit
 	int indexrow;
 
+	// Constructeur vide => instanciation (et initialisation)
 	public ClientManagedBean() {
+		// Instanciation
 		client = new Client();
 		produit = new Produit();
 		categorie = new Categorie();
 		ligneCommande = new LigneCommande();
+
+		// Création d'une nouvelle commande
 		commande = new Commande(Calendar.getInstance());
+
+		// Instenciation des listes
 		listeProduitsCmd = new ArrayList<Produit>();
 		listeLignesCmd = new ArrayList<LigneCommande>();
 		listeCommande = new ArrayList<Commande>();
+
+		// Association des listes qui font les liens entre les tables de la BDD
 		this.produit.setListeLigneCommandes(listeLignesCmd);
 		this.commande.setListeLignesCommandes(listeLignesCmd);
 		this.client.setListeCommande(listeCommande);
 	}
 
+	// Getters & Setters
 	public Client getClient() {
 		return client;
 	}
@@ -141,18 +158,27 @@ public class ClientManagedBean implements Serializable {
 	}
 
 	// Méthodes
+
+	// Méthode executée à la construction de la page
 	@PostConstruct
 	public void getAllCategories() {
+
+		// Initialisation de la liste de toutes les catégories
 		this.listeCategories = clientService.getAllCategories();
-		System.out.println("=================**>" + this.listeProduits);
+
+		// Initialisation de la liste des produits possédant le mot clef saisi
+		// dans la barre de recherche
 		this.listeProduits = clientService.getProductByKeyWordService(this.recherche);
-		System.out.println("=================**>" + this.listeProduits);
+
 	}
 
+	// Récupère la liste de tous les produits
 	public void getAllProd() {
 		this.listeProduits = clientService.getProductByKeyWordService("");
 	}
 
+	// Récupère la liste des produits possédant le mot clef saisi dans la barre
+	// de recherche
 	public void getProd() {
 		System.out.println("=================+>" + this.listeProduits);
 		this.listeProduits = clientService.getProductByKeyWordService(this.recherche);
@@ -160,115 +186,147 @@ public class ClientManagedBean implements Serializable {
 
 	}
 
+	// Récupère le contenu du panier
 	public void getCart() {
+
+		// Récupère la liste des lignes de commandes
 		List<LigneCommande> llcmd = this.commande.getListeLignesCommandes();
+
+		// Réinitialisation de la liste des produits (qui s'affiche dans le
+		// tableau
 		this.listeProduits.clear();
+
+		// On met les produits récupérés dans la liste des lignes de commande
+		// dans la liste de produits à afficher
 		for (LigneCommande lc : llcmd) {
 			this.listeProduits.add(lc.getProduit());
 		}
 	}
 
+	// Récupère la liste des produits correspondants à la catégorie sélectionnée
 	public String getProdByCat() {
 		this.listeProduits = clientService.getProductByCatService(Integer.parseInt(this.indexCat));
 		return null;
 	}
 
+	// Récupère la liste des produits contenant le mot saisi dans la barre de
+	// recherche
 	public String getByKeyWord() {
 		this.listeProduits = clientService.getProductByKeyWordService(this.recherche);
-		System.out.println(this.listeProduits);
+
+		// Retour à la page de recherhce pour afficher le résultat
 		return "Recherche";
 	}
 
+	// Méthode pour enregistrer la commande
 	public String order() {
 
+		// On associe le client à la commande
 		this.commande.setClient(this.client);
 
+		// Création de la liste des commandes passées par le client
 		List<Commande> listCmd = new ArrayList<Commande>();
 		listCmd = this.client.getListeCommande();
+
+		// Ajout de la commande passée dans la liste de toutes les commandes
 		listCmd.add(this.commande);
+
+		// Association du client et des commandes passées
 		this.client.setListeCommande(listCmd);
+
+		// Enregistrement de la commande dans la BDD
 		clientService.orderService(client);
-		// débiter les stocks
+
+		// débiter les stocks pour chacun des produits commandés
 		for (LigneCommande lc : this.listeLignesCmd) {
 
+			// On récupère un produit de la liste des lignes de commande
 			Produit p = new Produit();
 			p = lc.getProduit();
+
+			// Onrécupère la quantité du produit acheté par le client
 			int quantt = p.getQuantite();
 
+			// On récupère la quantité de produits disponible dans la BDD
 			int quanttStock = clientService.getProductByIdService(p).get(0).getQuantite();
-			System.out.println("///////////////////////////////////////////////////////////////////////");
-			System.out.println("quantt =>" + quantt);
-			System.out.println("quanttStock =>" + quanttStock);
+
+			// Nombre de produits restants après l'achat
 			p.setQuantite(quanttStock - quantt);
-			System.out.println(p);
+
+			// Mise à jour du produit dans la BDD après achat
 			int verif = adminService.updateProductService(p);
-			System.out.println("verif" + verif);
+
 		}
 
-		client = new Client();
-		produit = new Produit();
-		categorie = new Categorie();
-		ligneCommande = new LigneCommande();
-		commande = new Commande(Calendar.getInstance());
-		listeProduitsCmd = new ArrayList<Produit>();
-		listeLignesCmd = new ArrayList<LigneCommande>();
-		listeCommande = new ArrayList<Commande>();
+		// Réinitialisation des variables après les variables (même chose que
+		// dans le constructeur vide)
+		this.client = new Client();
+		this.produit = new Produit();
+		this.categorie = new Categorie();
+		this.ligneCommande = new LigneCommande();
+		this.commande = new Commande(Calendar.getInstance());
+		this.listeProduitsCmd = new ArrayList<Produit>();
+		this.listeLignesCmd = new ArrayList<LigneCommande>();
+		this.listeCommande = new ArrayList<Commande>();
 		this.produit.setListeLigneCommandes(listeLignesCmd);
 		this.commande.setListeLignesCommandes(listeLignesCmd);
 		this.client.setListeCommande(listeCommande);
 
-		listeProduits = clientService.getProductByKeyWordService("");
+		//On met la liste de tous les produits dans la liste à afficher
+		this.listeProduits = clientService.getProductByKeyWordService("");
 
+		//Retour à l'accueil
 		return "Accueil";
 	}
 
+	//Ajout du produit dans le panier
 	public String selectProduct() {
 
 		// Ajout du produit & de la commande dans la ligne commande
 		LigneCommande lc = new LigneCommande();
 		lc.setProduit(this.produit);
 		lc.setCommande(this.commande);
+		
+		//On récupère la quantité et le prix et on calcule le prix total concernant ce produit
 		int quantite = this.produit.getQuantite();
 		double prix = quantite * this.produit.getPrix();
+		
+		//On met la quantité et le prix calculé dans la ligne de commande
 		lc.setPrix(prix);
 		lc.setQuantite(quantite);
 
-		// Ajout de la ligne commande dans le produit
+		//On récupère la liste des lignes de commande (tous les produits du panier)
 		List<LigneCommande> llcmd = new ArrayList<LigneCommande>();
 		llcmd = this.produit.getListeLigneCommandes();
+		//Ajout de la nouvelle ligne de commande
 		llcmd.add(lc);
+		// Ajout de la ligne commande dans le produit
 		this.produit.setListeLigneCommandes(llcmd);
 
-		// Ajout de la ligne commande dans la commande
-
+		//On récupère la liste de ligne de commande dans la commande actuelle
 		llcmd = this.commande.getListeLignesCommandes();
+		//Ajout de la ligne de commande dans la liste
 		llcmd.add(lc);
+		// Ajout de la liste de ligne commande dans la commande
 		this.commande.setListeLignesCommandes(llcmd);
 
 		// Le produit est sélectionné
 		this.produit.setSelectionne(true);
 
-//		System.out.println(
-//				"====================================================================================================================");
-//		System.out.println(commande);
-//		for (LigneCommande lc1 : commande.getListeLignesCommandes()) {
-//			System.out.println(lc1.getProduit());
-//		}
-//		for (LigneCommande lc2 : commande.getListeLignesCommandes()) {
-//			System.out.println(lc2);
-//		}
-//		System.out.println(
-//				"====================================================================================================================");
-
 		// Regeneration de la liste de produits
 		listeProduits = clientService.getProductByKeyWordService("");
+		
+		//Retour à la page qui a appelé la méthode
 		return null;
 	}
 
+	//Suppression du produit
 	public String deleteProduct() {
-
-		this.listeLignesCmd.remove(indexrow);
 		
+		//Suppression du panier avec l'index du tableau
+		this.listeLignesCmd.remove(indexrow);
+
+		//Retour à la page ayant appelé la méthode
 		return null;
 	}
 }
